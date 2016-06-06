514081b53a6c391f9ad17d325a20b6f2f68c7a86
wuzhenli <wuzhenli@kongfz.com>
2016-06-06 09:28:26 +0800

    添加项目-Relam

---
+++ Demo_FrameWork/Demo_FrameWork/Class/SubPage/Realm/RealmViewController.h
@@ -0,0 +1,13 @@
+//
+//  RealmViewController.h
+//  Frame_USE
+//
+//  Created by kfz on 16/6/6.
+//  Copyright © 2016年 Jaly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RealmViewController : UIViewController
+
+@end
---
+++ Demo_FrameWork/Demo_FrameWork/Class/SubPage/Realm/RealmViewController.m
@@ -0,0 +1,37 @@
+//
+//  RealmViewController.m
+//  Frame_USE
+//
+//  Created by kfz on 16/6/6.
+//  Copyright © 2016年 Jaly. All rights reserved.
+//
+
+#import "RealmViewController.h"
+
+@interface RealmViewController ()
+
+@end
+
+@implementation RealmViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor cyanColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
